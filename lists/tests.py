78ce25d494e29d72416fd69252a7f38cd9944258
Andre Heringer <andregomesh@dcc.ufmg.br>
2020-09-11 17:52:37 -0300

    Apresentar Múltiplos Itens na Listagem

--- lists/tests.py
+++ lists/tests.py
@@ -55,4 +55,13 @@ class HomePageTest(TestCase):
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
+    
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
