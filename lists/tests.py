55d039c102cfa04ea7504d9943d0eea96632bfa3
Andre Heringer <andregomesh@dcc.ufmg.br>
2020-09-11 17:19:19 -0300

    Processando Requisição POST

--- lists/tests.py
+++ lists/tests.py
@@ -17,4 +17,8 @@ class HomePageTest(TestCase):
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
 
-        self.assertTemplateUsed(response, 'home.html') 
+        self.assertTemplateUsed(response, 'home.html')
+    
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
