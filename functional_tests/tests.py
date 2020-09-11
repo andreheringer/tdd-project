be20637faf90e709a1863739a6547c349bfec3c5
Andre Heringer <andregomesh@dcc.ufmg.br>
2020-09-11 18:03:20 -0300

    Isolando Testes Funcionais

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
-class NewVisitorTest(unittest.TestCase):
+
+
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):  
          self.browser = webdriver.Firefox(executable_path="/home/andre/Projects/tdd-project/geckodriver")
@@ -18,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self): 
 
         # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
-        self.browser.get('http://127.0.0.1:8000')
+        self.browser.get(self.live_server_url)
 
         # Ela nota que o título da página menciona TODO
         self.assertIn('To-Do', self.browser.title)
@@ -54,6 +56,3 @@ class NewVisitorTest(unittest.TestCase):
         # Ela visita a URL: a sua lista TODO ainda está armazenada
 
         # Satisfeita, ela vai dormir
-
-if __name__ == '__main__':
-    unittest.main()
