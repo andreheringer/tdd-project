597b1f41d29fd3bfa24f7e2cb0e6317e121599a3
Andre Heringer <andregomesh@dcc.ufmg.br>
2020-10-18 15:23:12 -0300

    Validação: Prevenindo itens vazios - PARTE 1

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -18,3 +18,13 @@ class FunctionalTest(StaticLiveServerTestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
+
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()  
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
