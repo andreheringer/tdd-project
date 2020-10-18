6d669d1407c8179a7099fd2cd13b239f4d71b016
Andre Heringer <andregomesh@dcc.ufmg.br>
2020-10-18 15:18:03 -0300

    Refatorando o Teste de Unidade

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,51 +1,5 @@
 from django.test import TestCase
 from lists.models import Item, List
-from lists.models import Item
-from django.urls import resolve
-from django.test import TestCase
-from django.http import HttpRequest
-from lists.views import home_page
-
-class NewListTest(TestCase):
-
-    def test_can_save_a_POST_request(self):
-        self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        new_list = List.objects.first()
-        self.assertRedirects(response, f'/lists/{new_list.id}/')
-
-class ListViewTest(TestCase):
-
-    def test_uses_list_template(self):
-        my_list = List.objects.create()
-        response = self.client.get(f'/lists/{my_list.id}/')
-        self.assertTemplateUsed(response, 'list.html')
-
-    def test_displays_only_items_for_that_list(self):
-        correct_list = List.objects.create()
-        Item.objects.create(text='itemey 1', list=correct_list)
-        Item.objects.create(text='itemey 2', list=correct_list)
-        other_list = List.objects.create()
-        Item.objects.create(text='other list item 1', list=other_list)
-        Item.objects.create(text='other list item 2', list=other_list)
-
-        response = self.client.get(f'/lists/{correct_list.id}/')
-
-        self.assertContains(response, 'itemey 1')
-        self.assertContains(response, 'itemey 2')
-        self.assertNotContains(response, 'other list item 1')
-        self.assertNotContains(response, 'other list item 2')
-    
-    def test_passes_correct_list_to_template(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-        response = self.client.get(f'/lists/{correct_list.id}/')
-        self.assertEqual(response.context['list'], correct_list)
 
 class ListAndItemModelsTest(TestCase):
 
