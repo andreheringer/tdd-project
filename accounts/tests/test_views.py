9df8050446d3fc1e2dd04f3d9245d4fa8e2d252c
Andre Heringer <andregomesh@dcc.ufmg.br>
2020-10-20 18:01:16 -0300

    Testando o envio de email: Test Double lib

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 import accounts.views
+from unittest.mock import patch
 
 class SendLoginEmailViewTest(TestCase):
 
@@ -29,3 +30,15 @@ class SendLoginEmailViewTest(TestCase):
         self.assertEqual(self.subject, 'Your login link for Superlists')
         self.assertEqual(self.from_email, 'noreply@superlists')
         self.assertEqual(self.to_list, ['edith@example.com'])
+
+    @patch('accounts.views.send_mail')
+    def test_sends_mail_to_address_from_post(self, mock_send_mail):
+        self.client.post('/accounts/send_login_email', data={
+            'email': 'edith@example.com'
+        })
+
+        self.assertEqual(mock_send_mail.called, True)
+        (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
+        self.assertEqual(subject, 'Your login link for Superlists')
+        self.assertEqual(from_email, 'noreply@superlists')
+        self.assertEqual(to_list, ['edith@example.com'])
