a0e9c8ddefbe67371d7124d20c3dbd1ae807e65b
Andre Heringer <andregomesh@dcc.ufmg.br>
2020-10-20 17:49:15 -0300

    Modelo Token

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,4 @@
-from django.db import models
-
+import uuid
 from django.db import models
 
 class User(models.Model):
@@ -9,3 +8,7 @@ class User(models.Model):
     USERNAME_FIELD = 'email'
     is_anonymous = False
     is_authenticated = True
+
+class Token(models.Model):
+    email = models.EmailField()
+    uid = models.CharField(default=uuid.uuid4, max_length=40)
